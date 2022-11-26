c96671323d87c0281dead6a783bd6e94f7dffde6
Hirake <Robin.berg@hotmail.nl>
2022-11-27 00:48:12 +0100

    Product Service Layer and annotations

--- src/main/java/com/robin/springbootbackend/product/ProductController.java
+++ src/main/java/com/robin/springbootbackend/product/ProductController.java
@@ -1,5 +1,6 @@
 package com.robin.springbootbackend.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,16 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/product")
 public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
 
     @GetMapping
-    public List<Product> hello(){
-        return List.of(
-                new Product("Mario Kart", 49.99, "Dit is Mario Kart")
-        );
+    public List<Product> getProducts(){
+        return productService.getProducts();
     }
+
 }
