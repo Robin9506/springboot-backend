a24ac4a6c702f214ca0362810f5d250e3527a9c2
Hirake <Robin.berg@hotmail.nl>
2022-11-27 11:29:07 +0100

    database connection and creating table with JPA

--- src/main/java/com/robin/springbootbackend/product/Product.java
+++ src/main/java/com/robin/springbootbackend/product/Product.java
@@ -1,6 +1,20 @@
 package com.robin.springbootbackend.product;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Product {
+    @Id
+    @SequenceGenerator(
+            name= "product_sequence",
+            sequenceName = "product_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_sequence"
+    )
     private Long id;
     private String name;
     private double price;
