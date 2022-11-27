4a1d047f03c6261c113ee3ea9b49c436c92af782
Hirake <Robin.berg@hotmail.nl>
2022-11-27 12:05:35 +0100

    add new product, also error message when there is already an entity with the same name

--- src/main/java/com/robin/springbootbackend/product/ProductController.java
+++ src/main/java/com/robin/springbootbackend/product/ProductController.java
@@ -1,9 +1,7 @@
 package com.robin.springbootbackend.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +18,11 @@ public class ProductController {
     @GetMapping
     public List<Product> getProducts(){
         return productService.getProducts();
+    }
+
+    @PostMapping
+    public void postProduct(@RequestBody Product product){
+        productService.addNewProduct(product);
     }
 
 }
--- src/main/java/com/robin/springbootbackend/product/ProductRepository.java
+++ src/main/java/com/robin/springbootbackend/product/ProductRepository.java
@@ -1,8 +1,14 @@
 package com.robin.springbootbackend.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("Select p FROM Product p WHERE p.name = ?1")
+    Optional<Product> findProductByName(String name);
 }
