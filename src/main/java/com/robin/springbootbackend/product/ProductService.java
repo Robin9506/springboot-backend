4f4e106ebd2ba70354105e946eeff3397c6bc41e
Hirake <Robin.berg@hotmail.nl>
2022-11-27 11:42:23 +0100

    productDAO and config

--- src/main/java/com/robin/springbootbackend/product/ProductService.java
+++ src/main/java/com/robin/springbootbackend/product/ProductService.java
@@ -1,5 +1,6 @@
 package com.robin.springbootbackend.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -7,10 +8,14 @@ import java.util.List;
 
 @Service
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+
+    }
     public List<Product> getProducts(){
-        return List.of(
-                new Product("Mario Kart", 49.99, "Dit is Mario Kart"),
-                new Product("Mario Party", 39.99, "Dit is Mario Party")
-        );
+        return productRepository.findAll();
     }
 }
