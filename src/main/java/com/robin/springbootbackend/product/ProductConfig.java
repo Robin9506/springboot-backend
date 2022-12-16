f748abd47da43fc1f50a408a5b16692b29b67abf
Hirake <Robin.berg@hotmail.nl>
2022-12-16 22:38:03 +0100

    product uses uuid and single one can get retrieved from uuid

--- src/main/java/com/robin/springbootbackend/product/ProductConfig.java
+++ src/main/java/com/robin/springbootbackend/product/ProductConfig.java
@@ -10,14 +10,8 @@ import java.util.List;
 public class ProductConfig {
     @Bean
     CommandLineRunner commandLineRunner(
-            ProductRepository productRepository){
-        return args -> {
-                    Product marioKart = new Product("Mario Kart", 49.99, "Dit is Mario Kart");
-                    Product marioParty = new Product("Mario Party", 39.99, "Dit is Mario Party");
+            ProductRepository productRepository) {
 
-            productRepository.saveAll(
-                    List.of(marioKart, marioParty)
-            );
-        };
+        return null;
     }
 }
